74fc819eccb48c4909a12380defba019f6c205b2
= <=>
2022-07-09 21:48:39 -0300

    Adicionando arquivos do projeto ao commit

---
+++ funcoes_recursivas/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace funcoes_recursivas
+{
+
+    class Program
+    {
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string caminho = @"C:\Users\FATIMA AIKAWA\Documents\CURSO C# DANILO APARECIDO\07 - FUNÇÕES RECURSIVAS - C# - TORNE-SE UM PROGRAMADOR\arquivos\arq" + numeroArquivo + ".txt";
+            if (File.Exists(caminho))
+            {
+                using (StreamReader arquivo = File.OpenText(caminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+
+                }
+            }
+
+            string caminho2 = @"C:\Users\FATIMA AIKAWA\Documents\CURSO C# DANILO APARECIDO\07 - FUNÇÕES RECURSIVAS - C# - TORNE-SE UM PROGRAMADOR\arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(caminho2))
+            {
+                using(StreamReader arquivo2 = File.OpenText(caminho2))
+                {
+                    LerArquivo(numeroArquivo + 1);
+                }
+            }
+
+        }
+
+        
+
+
+        static void Main(string[]args)
+            {
+            LerArquivo(1);
+            }
+        
+    }
+
+}
